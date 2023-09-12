aa61820edbc4429611e6f578554f4e6341a349ff
aayzhao <aaron.y.zhao1@gmail.com>
2023-09-12 15:28:52 +0000

    Deleted compiled binaries from tracking. New classwork on structs added

---
+++ classwork/structs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+typedef struct {
+    int num;
+    char word[20];
+    bool boolean;
+} mytype_t;
+
+int main()
+{
+    mytype_t my_struct = {20, "Hello, World", true};
+
+    printf("Struct num: %d\nStruct word: %s\nStruct bool: %s\n", my_struct.num, my_struct.word, my_struct.boolean ? "true" : "false");
+}
