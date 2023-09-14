93727a69bf1dd70e0451fd3c19b775aafc4f7f10
aayzhao <aaron.y.zhao1@gmail.com>
2023-09-14 17:41:24 +0000

    September 14th classwork

---
+++ classwork/sept14.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    size_t len;
+    char *str[1024];
+} string;
+
+int main ()
+{
+    char str[1024];
+
+    fgets(str, 1024, stdin);
+    ret = memchr(str, '\n', strlen(str));
+    string s = {strlen(str), {str}};
+
+    size_t size = s.len;
+
+    printf("\nSize of input string: %lu\n", size);
+    printf("Input string: %s\n", s.str);
+
+    
+
+    return EXIT_SUCCESS;
+}
