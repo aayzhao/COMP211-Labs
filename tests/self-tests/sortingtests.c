cb2799f9bc68508633ac0ff8c5e33ef64c9b164a
aayzhao <aaron.y.zhao1@gmail.com>
2023-09-26 22:32:58 +0000

    backup test files

---
+++ tests/self-tests/sortingtests.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../includes/output.h"
+#include "../includes/arrfill.h"
+#include "../includes/arraystruct.h"
+#include "../includes/algo.h"
+
+int inc(int x)
+{
+    int y = 0;
+    y += x;
+    return y;
+}
+
+int main()
+{
+    int arr[] = { 20, 1, 52, -1, -23, 2, 401, 5, 9, 102 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    //printf("Min value in array:\t%d\n", arr[arrmin(arr, size)]);
+    //printf("Max value in array:\t%d\n", arr[arrmax(arr, size)]);
+    //printf("Size of array:\t\t%d\n", size);
+    
+    heapsort(arr, 0, size - 1);
+    printf("Sorted array:");
+    printintarr(arr, size);
+    
+    long larr[] = { 20, 1, 52, -1, -1000, 3, 13, 204, 509192, -21, -21, -21 };
+    int lsize = sizeof(larr) / sizeof(larr[0]);
+
+    printlongarr(larr, lsize);
+    lheapsort(larr, 0, lsize - 1);
+    printlongarr(larr, lsize);
+    
+    printf("%d\n", lbinsearch(larr, 0, 11, -21));
+    printf("%d\n", binsearch(arr, 0, 9, 3));
+
+    printf("Contains -21:\t%s\n", lbinsearch(larr, 0, 11, -21) != -1 ? "true" : "false");
+    printf("Contains 3:\t%s\n", binsearch(arr, 0, 9, 3) != -1 ? "true" : "false");
+    
+    int* ptr = (int*) malloc(100*sizeof(int));
+    printf("%p\n", ptr);
+
+    free(ptr);
+    printf("%p\n", ptr);
+    
+    printf("y val: %d\n", inc(10)); // 10
+    printf("y val: %d\n", inc(30)); // 40
+    return EXIT_SUCCESS;
+}
--- tests/test.c
+++ tests/test.c
@@ -15,34 +15,14 @@ int inc(int x)
 
 int main()
 {
-    int arr[] = { 20, 1, 52, -1, -23, 2, 401, 5, 9, 102 };
-    int size = sizeof(arr) / sizeof(arr[0]);
-    
-    //printf("Min value in array:\t%d\n", arr[arrmin(arr, size)]);
-    //printf("Max value in array:\t%d\n", arr[arrmax(arr, size)]);
-    //printf("Size of array:\t\t%d\n", size);
-    
-    heapsort(arr, 0, size - 1);
-    printf("Sorted array:");
-    printintarr(arr, size);
-    
-    long larr[] = { 20, 1, 52, -1, -1000, 3, 13, 204, 509192, -21, -21, -21 };
-    int lsize = sizeof(larr) / sizeof(larr[0]);
-
-    printlongarr(larr, lsize);
-    lheapsort(larr, 0, lsize - 1);
-    printlongarr(larr, lsize);
-    
-    printf("%d\n", lbinsearch(larr, 0, 11, -21));
-    printf("%d\n", binsearch(arr, 0, 9, 3));
-
-    printf("Contains -21:\t%s\n", lbinsearch(larr, 0, 11, -21) != -1 ? "true" : "false");
-    printf("Contains 3:\t%s\n", binsearch(arr, 0, 9, 3) != -1 ? "true" : "false");
-    
     int* ptr = (int*) malloc(100*sizeof(int));
     printf("%p\n", ptr);
-
+    int a = 20;
     free(ptr);
+    ptr = NULL;
+    
+    printf("%d\n", *&a);
+    printf("null pointer: %s\n", ptr == NULL ? "true" : "false");
     printf("%p\n", ptr);
     
     printf("y val: %d\n", inc(10)); // 10
