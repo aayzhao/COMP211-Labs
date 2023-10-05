11cdb49f94f2a3cad28fc5baf4ef99be8e199559
aayzhao <aaron.y.zhao1@gmail.com>
2023-10-05 22:12:25 +0000

    limits testing

--- classwork/print_limits.c
+++ classwork/print_limits.c
@@ -7,11 +7,11 @@ int main()
     printf("\n\n");
     
     printf("Integer min: \t\t\t%d\n", INT_MIN);
-    printf("Integer max: \t\t\t%d\n", INT_MAX);
-    printf("Unsigned Integer max: \t\t%u\n", UINT_MAX);
+    printf("Integer max: \t\t\t %d\n", INT_MAX);
+    printf("Unsigned Integer max: \t\t %u\n", UINT_MAX);
     printf("Long Integer min: \t\t%ld\n", LONG_MIN);
-    printf("Long Integer max: \t\t%ld\n", LONG_MAX); 
-    printf("Unsigned Long Integer max: \t%lu\n", ULONG_MAX);
+    printf("Long Integer max: \t\t %ld\n", LONG_MAX); 
+    printf("Unsigned Long Integer max: \t %lu\n", ULONG_MAX);
     
     printf("\n\n");
 
